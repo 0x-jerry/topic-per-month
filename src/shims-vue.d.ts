95da443794d763645b1bc7f241054daa45a0042b
Gem Xli <x.li.gem@gmail.com>
2021-06-02 15:26:46 +0800

    chore: remove duplicate declare

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -9,9 +9,3 @@ declare module '*.md' {
   const Component: ComponentOptions
   export default Component
 }
-
-declare module 'virtual:generated-pages' {
-  import { RouteRecordRaw } from 'vue-router'
-  const v: RouteRecordRaw[]
-  export default v
-}
