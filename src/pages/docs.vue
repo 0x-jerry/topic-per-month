2b4ae16d06fcf01f6332dce9ecc2675aa318bc39
Gem Xli <x.li.gem@gmail.com>
2021-06-04 14:09:25 +0800

    perf: add goback button

--- src/pages/docs.vue
+++ src/pages/docs.vue
@@ -1,3 +1,23 @@
 <template>
+  <div>
+    <a @click="$router.back()"> goback </a>
+  </div>
+  <div>title: {{ article?.title }}</div>
+  <div>tags: {{ article?.tags }}</div>
+  <hr />
   <router-view />
 </template>
+
+<script lang="ts" setup>
+import { useRoute } from 'vue-router'
+import conf from 'virtual:site'
+import { computed } from '@vue/runtime-core'
+
+const r = useRoute()
+
+const article = computed(() => {
+  const routePath = r.path.split('/').pop()
+
+  return conf.articles.find((a) => a.routePath === routePath)
+})
+</script>
