1ff98a17acfbf6b55f26f1f25331ee8ab6c80d32
Gem Xli <x.li.gem@gmail.com>
2021-06-16 22:21:46 +0800

    fix: build error

--- src/components/VGiscus.vue
+++ src/components/VGiscus.vue
@@ -6,23 +6,27 @@
 import { onMounted, ref } from 'vue-demi'
 const el = ref<HTMLDivElement>()
 
-const script = document.createElement('script')
-
-script.src = 'https://giscus-henna.vercel.app/client.js'
-script.crossOrigin = 'anonymous'
-script.async = true
-
-script.setAttribute('data-repo', 'cwxyz007/topic-per-month')
-script.setAttribute('data-repo-id', 'MDEwOlJlcG9zaXRvcnkzNzI2NzI5NzA=')
-script.setAttribute('data-category', 'General')
-script.setAttribute('data-category-id', 'MDE4OkRpc2N1c3Npb25DYXRlZ29yeTMzMDMwODM4')
-script.setAttribute('data-mapping', 'og:title')
-script.setAttribute('data-reactions-enabled', '0')
-script.setAttribute('data-theme', 'light')
-
 onMounted(() => {
   if (el.value) {
+    const script = createScript()
     el.value.appendChild(script)
   }
 })
+
+function createScript() {
+  const script = document.createElement('script')
+
+  script.src = 'https://giscus-henna.vercel.app/client.js'
+  script.crossOrigin = 'anonymous'
+  script.async = true
+
+  script.setAttribute('data-repo', 'cwxyz007/topic-per-month')
+  script.setAttribute('data-repo-id', 'MDEwOlJlcG9zaXRvcnkzNzI2NzI5NzA=')
+  script.setAttribute('data-category', 'General')
+  script.setAttribute('data-category-id', 'MDE4OkRpc2N1c3Npb25DYXRlZ29yeTMzMDMwODM4')
+  script.setAttribute('data-mapping', 'og:title')
+  script.setAttribute('data-reactions-enabled', '0')
+  script.setAttribute('data-theme', 'light')
+  return script
+}
 </script>
